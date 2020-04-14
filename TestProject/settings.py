cd10ddcf45ddc22fce8345abd2f432ac876dfdfd
Abdoulaye Thiam <xpa700@hotmail.com>
2020-04-14 23:49:31 +0200

    Ready to deploy

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '40lwi3+5c$db==%6g)$dbsogij2t@wxagvoqry1v)lbk!g=ljm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
