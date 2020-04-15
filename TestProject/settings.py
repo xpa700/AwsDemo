c64b25f860196b2f1a67061d1f0d6cab4dd6570e
Abdoulaye Thiam <xpa700@hotmail.com>
2020-04-15 15:09:22 +0200

    Reverted Deploy mode.

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '40lwi3+5c$db==%6g)$dbsogij2t@wxagvoqry1v)lbk!g=ljm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
