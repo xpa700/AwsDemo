5abfa181aefe85d8eb74ec6b662a7b972f6698c0
Abdoulaye Thiam <xpa700@hotmail.com>
2020-04-15 14:58:01 +0200

    Added the template.

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TestProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'TestProject/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
